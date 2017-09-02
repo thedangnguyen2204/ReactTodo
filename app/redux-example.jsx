f367c878036df218f83986b3f0e2817649663772
Dang The <thedangnguyen2204@gmail.com>
2017-09-02 19:55:40 +0700

    Add support for location fetching

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,5 +1,5 @@
 var redux = require('redux');
-
+var axios = require('axios');
 //Name reducer and action generators
 //-----------------------------
 var nameReducer = (state = 'Anonymous', action) => {
@@ -80,10 +80,54 @@ var removeMovie = (id) => {
 	}
 };
 
+//Map reducer and action generators
+//-----------------------------
+var mapReducer = (state = {isFetching: false, url: undefined}, action) => {
+	switch (action.type) {
+		case 'START_LOCATION_FETCH':
+			return {
+				isFetching: true,
+				url: undefined
+			};
+		case 'COMPLETE_LOCTION_FETCH':
+			return {
+				isFetching: false,
+				url: action.url
+			};
+		default:
+			return state;
+	}
+};
+
+var startLocaitonFetch = () => {
+	return {
+		type: 'START_LOCATION_FETCH'
+	};
+};
+
+var completeLocationFetch = (url) => {
+	return {
+		type: 'COMPLETE_LOCTION_FETCH',
+		url: url
+	}
+};
+
+var fetchLocation = () => {
+	store.dispatch(startLocaitonFetch());
+
+	axios.get('http://ipinfo.io').then (function (res) {
+		var loc = res.data.loc;
+		var baseUrl = 'http://maps.google.com?q=';
+
+		store.dispatch(completeLocationFetch(baseUrl + loc));
+	})
+};
+
 var reducer = redux.combineReducers({
 	name: nameReducer,
 	hobbies: hobbiesReducer,
-	movies: moviesReducer
+	movies: moviesReducer,
+	map: mapReducer
 });
 
 var store = redux.createStore(reducer);
@@ -91,15 +135,21 @@ var store = redux.createStore(reducer);
 //Subscribe to changes
 store.subscribe(() => {
 	var state = store.getState();
-	console.log('Name is ', state.name);
-	document.getElementById('app').innerHTML = state.name;
 
 	console.log('New state is', state);
+
+	if (state.map.isFetching) {
+		document.getElementById('app').innerHTML = 'Loading...';
+	} else if (state.map.url) {
+		document.getElementById('app').innerHTML = '<a href="' + state.map.url + '" target="_blank">View your location</a>';
+	}
 });
 // unsubscribe();
 
 var currentState = store.getState();
 console.log('currentState', currentState);
+
+fetchLocation();
 
 store.dispatch(changeName('Jon Snow'));
 
